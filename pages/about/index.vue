81393ae28cb22b574d807f79a9e3031f1fbe7c2e
Pinal Bhatt <pinal.bhatt@weightwatchers.com>
2018-11-03 06:39:41 -0400

    Logo and about page

---
+++ pages/about/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        PBDesk.com
+      </h1>
+      <h2 class="subtitle">
+        PBDesk.com 2019 Nuxt Project - About Page!
+      </h2>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+};
+</script>
+
+<style scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,20 +3,15 @@
     <div>
       <logo/>
       <h1 class="title">
-        pbdesk-com
+        PBDesk.com
       </h1>
       <h2 class="subtitle">
-        My solid Nuxt.js project
+        PBDesk.com 2019 Nuxt Project - Coming Soon!
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+        <nuxt-link 
+          to="/about" 
+          class="links">About</nuxt-link>
       </div>
     </div>
   </section>
