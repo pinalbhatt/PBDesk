df80906e69990cd7d6b4d94452d39edb831e030c
Pinal Bhatt <pinal.bhatt@weightwatchers.com>
2018-11-03 07:41:26 -0400

    CNAME for ghpages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,7 @@
 const pkg = require('./package');
 
-const routerBase =
-  process.env.DEPLOY_ENV === 'GH_PAGES'
-    ? {
-        router: {
-          base: '/PBDesk/',
-        },
-      }
-    : {};
-
 module.exports = {
   mode: 'universal',
-  ...routerBase,
   /*
   ** Headers of the page
   */
